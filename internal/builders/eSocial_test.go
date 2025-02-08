e55a05e0c092d95694b402cade41ff47aa96837d
Avelino Bego <avelino.bego@gmail.com>
2025-02-08 01:55:49 -0300

    testes

--- internal/builders/eSocial_test.go
+++ internal/builders/eSocial_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/xml"
 	"log"
 	"os"
+	"sped/internal/builders"
+	"sped/internal/schemas"
 	"testing"
 
 	"github.com/joho/godotenv"
@@ -34,5 +36,29 @@ func TestMain(m *testing.M) {
 }
 
 func TestCreateEvtInfoEmpregador(t *testing.T) {
-	// esocial = bu
+
+	inclusao := builders.New[schemas.Inclusao]()
+
+	infoEmpregador := builders.New[schemas.InfoEmpregador](
+		builders.With("Inclusao", inclusao),
+	)
+
+	ideEmpregador := builders.New[schemas.IdeEmpregador](
+		builders.With("NrInsc", "200"),
+		builders.With("TpInsc", "1"),
+	)
+
+	evtInfoEmpregador := builders.New[schemas.EvtInfoEmpregador](
+		builders.With("InfoEmpregador", infoEmpregador),
+		builders.With("IdAttr", "1234567890"),
+		builders.With("IdeEvento", "ID16944301890"),
+		builders.With("IdeEmpregador", ideEmpregador),
+	)
+
+	var iface any = evtInfoEmpregador
+	esocial = builders.New[schemas.ESocial](
+		builders.With("Evento", &iface),
+		builders.With("Namespace", "http://www.esocial.gov.br/schema/lote/eventos/envio/v1_2_3"),
+	)
+
 }
